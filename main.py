57b836de270e7c1a0f835c789958d516e4db48d1
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-06 13:48:32 +0700

    Using os.path.exists() function to check if a file exists

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 if __name__ == "__main__":
-  print("Python Check If File Exists")
+  # Using os.path.exists() function to check if a file exists
+
+  import os.path
+
+  print(os.path.exists("readme.txt"))
+
+  from os.path import exists as file_exist
+
+  print(file_exist("readme.txt"))
